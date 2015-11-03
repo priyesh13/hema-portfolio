c683019c44ef79f38b6969ee4cd5b7050465fb03
Priyesh Mistry <priyeshmistry@Priyeshs-MacBook-Pro-2.local>
2015-11-03 15:29:08 +0000

    Create angular app basis

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hemaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hemaApp
+ */
+angular.module('hemaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hemaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hemaApp
+ */
+angular.module('hemaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
